2b6d0853bc39aef2f5aa4aabd06ce80d44d4c7e5
HuynhCuongg <huynhcuongvan2004@gmail.com>
2025-03-31 15:11:57 +0700

    Quest

---
+++ Assets/_Scripts/Quest/QuestItem.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestItem : MonoBehaviour
+{
+    public string QuetsItemName; // Tên Quest
+    public int questTargetAmount; // số lượng cần tìm
+    public int currentAmount ; //số lượng hiện tại 
+    public string TargetItemtag; // tag của item cần tìm
+
+    // Kiểm tra hoàn thành
+   
+    public bool IsComplete()
+    {
+        return currentAmount >= questTargetAmount;
+    }
+
+    // Cập nhật số lượng item
+    public void UpdateQuestProgress()
+    {
+        currentAmount++;
+    }
+
+}
---
+++ Assets/_Scripts/Quest/StoryPaper.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class StoryPaper : MonoBehaviour
+{
+  
+    public Transform playerCamera;
+    public float raycastRange = 3f;
+    public GameObject Panelnote; // UI Panel chứa cốt truyện
+    private GameObject currentItem;
+    private bool isReading = false;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            if (Panelnote.activeSelf)
+            {
+                CloseStory();
+            }
+            else
+            {
+                CheckForItem();
+            }
+        }
+    }
+
+    void CheckForItem()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(playerCamera.position, playerCamera.forward, out hit, raycastRange))
+        {
+            if (hit.collider.CompareTag("Note")) // Đặt tag cho vật phẩm
+            {
+                currentItem = hit.collider.gameObject;
+                currentItem.SetActive(false); // Ẩn vật phẩm
+                ShowStory();
+            }
+        }
+    }
+
+    void ShowStory()
+    {
+        Panelnote.SetActive(true);
+        isReading = true;
+        Time.timeScale = 0f; // Dừng thời gian
+    }
+
+    void CloseStory()
+    {
+        Panelnote.SetActive(false);
+        if (currentItem != null)
+        {
+            currentItem.SetActive(true); // Hiện lại vật phẩm
+            currentItem = null;
+        }
+        isReading = false;
+        Time.timeScale = 1f; // Tiếp tục thời gian
+    }
+}
+
+
+
