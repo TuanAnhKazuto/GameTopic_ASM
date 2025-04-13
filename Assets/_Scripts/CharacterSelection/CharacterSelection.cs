626f5f4193d0261e9cff7caa1fd2290dc2c8b5ca
HAUMEOOOO <haumdps40975@gmail.com>
2025-04-14 02:37:22 +0700

    selection ship update

--- Assets/_Scripts/CharacterSelection/CharacterSelection.cs
+++ Assets/_Scripts/CharacterSelection/CharacterSelection.cs
@@ -1,6 +1,32 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CharacterSelection : MonoBehaviour
 {
-    
+    public GameObject[] character;
+    public int selectedCharacter = 0;
+
+    public void NextCharacter()
+    {
+        character[selectedCharacter].SetActive(false);
+        selectedCharacter = (selectedCharacter + 1) % character.Length;
+        character[selectedCharacter].SetActive(true);
+    }
+
+    public void PreviousCharacter()
+    {
+        character[selectedCharacter].SetActive(false);
+        selectedCharacter--;
+        if (selectedCharacter < 0)
+        {
+            selectedCharacter += character.Length;
+        }
+        character[selectedCharacter].SetActive(true);
+    }
+
+    public void StartGame()
+    {
+        PlayerPrefs.SetInt("selectedCharacter", selectedCharacter);
+        SceneManager.LoadScene(3, LoadSceneMode.Single);
+    }
 }
